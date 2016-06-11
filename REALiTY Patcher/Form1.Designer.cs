66ca8bfcc06253875a1296ea84669e40aab0b168
Rottweiler <rottweiler.hackhound@gmail.com>
2016-06-11 20:39:14 +0200

    REALiTY Patcher UI

---
+++ REALiTY Patcher/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace REALiTY_Patcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(62)))), ((int)(((byte)(67)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(29, 226);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 13);
+            this.textBox1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(29, 254);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(117, 24);
+            this.panel1.TabIndex = 1;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Location = new System.Drawing.Point(156, 254);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(117, 24);
+            this.panel2.TabIndex = 1;
+            this.panel2.Click += new System.EventHandler(this.panel2_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Location = new System.Drawing.Point(234, 220);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(39, 25);
+            this.panel3.TabIndex = 1;
+            this.panel3.Click += new System.EventHandler(this.panel3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::REALiTY_Patcher.Properties.Resources.REALiTY_UI_FINAL;
+            this.ClientSize = new System.Drawing.Size(300, 301);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ REALiTY Patcher/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+/// <summary>
+/// To make buttons work as they should, add a Panel the size of the button, and make the background color Transparent
+/// Then just add an event handler to "Panel.Click"
+/// </summary>
+
+namespace REALiTY_Patcher
+{
+    public partial class Form1 : Form
+    {
+
+        #region "Movable form"
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        #endregion
+
+        public string filename;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Movable form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        /// <summary>
+        /// Open file (btnPlus)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel3_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Title = Text;
+                ofd.Filter = "Executable *.exe|*.exe";
+                ofd.ShowDialog();
+                if (!string.IsNullOrEmpty(ofd.FileName))
+                {
+                    this.filename = ofd.FileName;
+                    textBox1.Text = this.filename.Substring(this.filename.LastIndexOf("\\"));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Patch (btnPatch)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel2_Click(object sender, EventArgs e)
+        {
+            Patch(this.filename);
+        }
+
+        /// <summary>
+        /// Exit (btnClose)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            GC.Collect();
+            Environment.Exit(0);
+        }
+
+        private void Patch(string file)
+        {
+            MessageBox.Show(this.filename + " patched!");
+        }
+    }
+}
